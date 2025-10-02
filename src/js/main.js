1191685e75e83f997bb49aa5ec24cc272ed32c77
CarlosSDT1 <csiguenzadt2@gmail.com>
2025-10-02 10:09:29 +0200

    main

--- src/js/main.js
+++
@@ -1,7 +0,0 @@
-import "../scss/style.scss"
-
-
-import * as bootstrap from 'bootstrap'
-import { renderHeader } from "../components/header"
-import { renderContent } from "../components/content";
-import { renderFooter } from "../components/footer";
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import "../scss/style.scss"
+
+
+import * as bootstrap from 'bootstrap'
+import { renderHeader } from "./components/header"
+import { renderContent } from "./components/content";
+import { renderFooter } from "./components/footer";
